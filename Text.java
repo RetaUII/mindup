8180bbc8d095161198de5ee8aeccb842d680ab7f
RetaG <88938568+RetaUII@users.noreply.github.com>
2021-08-14 17:10:52 +0100

    Update Text.java

--- Text.java
+++ Text.java
@@ -1,5 +1,5 @@
 public class Text {
     public static void main(String[] args) {
-        System.out.println("Hi this is being printed ");
+        System.out.println("Welcome To Mind Up.");
     }
 }
